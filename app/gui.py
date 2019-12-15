90a172f403ead9872282649c69080dfcdb5bdc57
svediNL <sdsdsven@gmail.com>
2019-12-15 14:45:14 +0100

    Update gui.py
    small fix  to autoscaling of plot (visually better)
    disable signal generator

--- app/gui.py
+++ app/gui.py
@@ -376,8 +376,8 @@ def animate(i):
         valH[1,n]= valH[1,n-1]
 
     # ADD VALUES TO BUFFERS
-    # tmpVal = app.arduino.readCommand("GET_MOISTURE")
-    tmpVal = str( valM[1,0] + 1 )
+    tmpVal = app.arduino.readCommand("GET_MOISTURE")
+    # tmpVal = str( valM[1,0] + 1 )
     app.moisture_var.set(tmpVal)
 
     try:
@@ -387,8 +387,8 @@ def animate(i):
     else:
         valM[1,0] = float(tmpVal)
 
-    #tmpVal = app.arduino.readCommand("GET_TEMP")
-    tmpVal = str( (valH[1,0]+1) % 2 )
+    tmpVal = app.arduino.readCommand("GET_TEMP")
+    #tmpVal = str( (valH[1,0]+1) % 2 )
     app.temperature_var.set(tmpVal)
 
     try:
@@ -397,9 +397,6 @@ def animate(i):
         valH[1,0] = float(0)
     else:
         valH[1,0] = float(tmpVal)
-
-    # print(valM[1,0])
-    # print(valH[1,0])
 
     # reverse value array for neatness
     valMneat = np.flip(valM, 1)
@@ -412,8 +409,8 @@ def animate(i):
 #    UPDATE MOUSTURE PLOT
     moistPlot.clear()
 
-    moistPlot.set_ylim([ min( 9, min(valMneat[1 , BUFF_LEN-BUFF_FILL:BUFF_LEN])*1.1), 
-                    max(11, max(valMneat[1, BUFF_LEN-BUFF_FILL : BUFF_LEN])*1.1) ])
+    moistPlot.set_ylim([ min( 0, min(valMneat[1 , BUFF_LEN-BUFF_FILL:BUFF_LEN])*1.1), 
+                    max(1, max(valMneat[1, BUFF_LEN-BUFF_FILL : BUFF_LEN])*1.1) ])
     # moistPlot.set_ylim([0,100])
 
     moistPlot.set_ylabel("% of FSV [%]")
@@ -426,8 +423,8 @@ def animate(i):
 #   UPDATE TEMOERATURE PLOT
     heatPlot.clear()
 
-    heatPlot.set_ylim([ min( 9, min(valHneat[1 , BUFF_LEN-BUFF_FILL:BUFF_LEN])*1.1), 
-                        max(11, max(valHneat[1, BUFF_LEN-BUFF_FILL : BUFF_LEN])*1.1) ])
+    heatPlot.set_ylim([ min( 0, min(valHneat[1 , BUFF_LEN-BUFF_FILL:BUFF_LEN])*1.1), 
+                        max(1, max(valHneat[1, BUFF_LEN-BUFF_FILL : BUFF_LEN])*1.1) ])
     # heatPlot.set_ylim([15,60])
 
     heatPlot.set_ylabel("temp [*C]")
