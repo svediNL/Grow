aa7a3ce6f6ac8693d6f07458037b043e65d312f7
svediNL <sdsdsven@gmail.com>
2024-11-02 13:47:01 +0100

    Add option to switch between dark / light theme

--- app/configuration.py
+++ app/configuration.py
@@ -84,21 +84,43 @@ LOG_NAME	= 'myLog.csv'
 
 
 ## G U I   D E S I G N
-BG_MAIN			= "gray30"	#"gray93"
-BG_SUB 			= "gray33"	#"gray96"
-BG_SUBSUB 		= "white"	#"white"
-BG_TOG_A		= BG_SUB	#"gray96"
-BG_TOG_B 		= "gray44"	#"gray93"
-BG_TAB 			= "gray44"	#"gray85"
-BG_TAB_ACTIVE 	= BG_SUB	#"gray96"
-BG_ENTRY		= "gray60"	#"white"
-BG_BUTTON		= "black"
-BG_CHECK		= "gray40"
-BG_SEL   = "gray77"
+THEME_DARK		= False
 
-FG_TEXT			= "white"	#"black"
-FG_TEXT2			= "black"
-FG_ENTRY		= "white"
+if THEME_DARK:
+	BG_MAIN			= "gray30"
+	BG_SUB 			= "gray33"
+	BG_SUBSUB 		= "white"
+	BG_TOG_A		= BG_SUB
+	BG_TOG_B 		= "gray44"
+	BG_TAB 			= "gray44"
+	BG_TAB_ACTIVE 	= BG_SUB
+	BG_ENTRY		= "gray60"
+	BG_BUTTON		= "black"
+	BG_CHECK		= "gray40"
+	BG_SEL   		= "gray77"
+
+	FG_TEXT			= "white"
+	FG_TEXT2		= "black"
+	FG_ENTRY		= "white"
+
+else:
+	BG_MAIN			= "gray93"
+	BG_SUB 			= "gray96"
+	BG_SUBSUB 		= "white"
+	BG_TOG_A		= "gray96"
+	BG_TOG_B 		= "gray93"
+	BG_TAB 			= "gray85"
+	BG_TAB_ACTIVE 	= "gray96"
+	BG_ENTRY		= "white"
+	BG_BUTTON		= "black"
+	BG_CHECK		= "gray40"
+	BG_SEL   		= "gray77"
+
+	FG_TEXT			= "black"
+	FG_TEXT2		= "black"
+	FG_ENTRY		= "white"
+
+
 
 
 THERMO_PLOT_CMAP = ['r', 'g', 'b', 'm', 'k']
